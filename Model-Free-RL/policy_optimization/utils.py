f47dcfb7e2c42a42f044c4f781998cac28d16617
Lily <liyo7397@colorado.edu>
2020-11-04 15:04:02 -0700

    Add tf.summary function.

--- Model-Free-RL/policy_optimization/utils.py
+++ Model-Free-RL/policy_optimization/utils.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
+import os
 
 
 def initial_state(env):
 
     return tf.constant(env.reset(), dtype=tf.float32)
 
-def convert_batchTensor(state):
+def insert_axis0Tensor(state):
 
     state = tf.constant(state, tf.float32)
     recent_state = tf.expand_dims(state, axis=0)
@@ -19,6 +20,35 @@ def initial_policyVar():
     rewards = tf.TensorArray(dtype=tf.int32, size=0, dynamic_size=True)
 
     return prob_action, values, rewards
+
+def insert_axis1Tensor(*args):
+
+    return [tf.expand_dims(x, 1) for x in args]
+
+def create_writer(log_dir='logs/'):
+
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+
+    summary_dir = log_dir+'/summary'
+    writer = tf.summary.create_file_writer(summary_dir)
+
+    return writer
+
+def write_summary(writer, episode, score_metric, loss_metric):
+
+    with writer.as_default():
+        tf.summary.scalar("Episode", episode, step=episode)
+        tf.summary.scalar("Loss", loss_metric.result(), step=episode)
+        tf.summary.scalar("Average score", score_metric.result(), step=episode)
+
+
+
+
+
+
+
+
 
 
 
