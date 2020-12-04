a0df43894bd320a9bcf61dcbf32497d8347ca512
Lily <liyo7397@colorado.edu>
2020-12-04 15:19:49 -0700

    Add the class for writing summary in tensorboard.

--- src/policy_optimization/utils.py
+++ src/policy_optimization/utils.py
@@ -25,8 +25,9 @@ def initial_tensorArray():
     values = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
     rewards = tf.TensorArray(dtype=tf.int32, size=0, dynamic_size=True)
     entropy = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
+    terminal = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
 
-    return prob_action, values, rewards, entropy
+    return prob_action, values, rewards, entropy, terminal
 
 def insert_axis1Tensor(*args):
 
@@ -44,23 +45,79 @@ def nn_input_shape(env_game, atari=False):
     return state.shape
 
 '''File'''
+class Writer:
 
-def create_writer(log_dir='logs/'):
+    def __init__(self):
 
-    if not os.path.exists(log_dir):
-        os.mkdir(log_dir)
+        self.loss_metric = tf.keras.metrics.Mean('loss', dtype=tf.float32)
+        self.score_metric = tf.keras.metrics.Mean('scores', dtype=tf.float32)
+        self.writer = self.create_writer()
 
-    summary_dir = log_dir+'/summary'
-    writer = tf.summary.create_file_writer(summary_dir)
+    def update_state(self, loss, rewards):
 
-    return writer
+        # Record the loss and rewards for tensorboard
+        self.loss_metric.update_state(loss)
+        self.score_metric.update_state(rewards)
 
-def write_summary(writer, episode, score_metric, loss_metric):
+    def create_writer(self, log_dir='logs/'):
 
-    with writer.as_default():
-        tf.summary.scalar("Episode", episode, step=episode)
-        tf.summary.scalar("Loss", loss_metric.result(), step=episode)
-        tf.summary.scalar("Average score", score_metric.result(), step=episode)
+        if not os.path.exists(log_dir):
+            os.mkdir(log_dir)
+
+        summary_dir = log_dir+'/summary'
+        writer = tf.summary.create_file_writer(summary_dir)
+
+        return writer
+
+    def write_summary(self, episode):
+
+        with self.writer.as_default():
+            tf.summary.scalar("Episode", episode, step=episode)
+            tf.summary.scalar("Loss", self.loss_metric.result(), step=episode)
+            tf.summary.scalar("Average score", self.score_metric.result(), step=episode)
+
+        self.loss_metric.reset_states()
+        self.score_metric.reset_states()
+
+class Memory():
+
+    def __init__(self):
+
+        self.prob_action = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
+        self.critic_values = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
+        self.rewards = tf.TensorArray(dtype=tf.int32, size=0, dynamic_size=True)
+        self.entropies = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
+        self.terminal = tf.TensorArray(dtype=tf.int32, size=0, dynamic_size=True)
+
+
+    def experiance(self, action, reward, entropy):
+
+        self.action = action
+        self.reward = reward
+        self.entropy = entropy
+
+    def training_data(self, t, prob_a, critic_val, done):
+        self.prob_action = self.prob_action.write(t, prob_a[0, self.action])
+        self.critic_values = self.critic_values.write(t, tf.squeeze(critic_val))
+        self.rewards = self.rewards.write(t, self.reward)
+        self.entropies = self.entropies.write(t, self.entropy)
+        self.terminal = self.terminal.write(t, done)
+
+    def to_stack(self):
+
+        self.prob_action = self.prob_action.stack()
+        self.critic_values = self.critic_values.stack()
+        self.rewards = self.rewards.stack()
+        self.entropies = self.entropies.stack()
+        self.terminal = self.terminal.stack()
+
+    def concat(self, other):
+
+        self.prob_action = tf.concat([self.prob_action, other.prob_action], 0)
+        self.critic_values = tf.concat([self.critic_values, other.critic_values], 0)
+        self.rewards = tf.concat([self.rewards, other.rewards], 0)
+        self.entropies = tf.concat([self.entropies, other.entropies], 0)
+        self.terminal = tf.concat([self.terminal, other.terminal], 0)
 
 
 
